9475f667c9297cd10fc8e9635c1639d201bb21e9
buthowz <sunit_lips@yahoo.com>
2019-06-13 08:56:00 -0700

    Updated

--- ObstacleAvoidance/ObstacleAvoidanceTest.cpp
+++ ObstacleAvoidance/ObstacleAvoidanceTest.cpp
@@ -17,15 +17,11 @@ int main() {
 	ObstacleAvoidance* car = new ObstacleAvoidance(a);
 	assert(car->clearAhead(11, 15) == true); // tests clearAhead method
 	
-	//cout<< car->getAGroup(1);
-	// test make_groups
-	//assert(car->getGroupLength() == 2);
-	//assert((car->getGroups().size()) == 10);
-	
 	// testing group_clear()
 	assert(car->groupClear(a) == false);
 	cout << "Number of Groups: "<< car->getGroups().size() << endl;
 
+	// testing makeGroups()
 	vector<float> b = {4, 4, 5, 7, 7, 8, 9, 10, 10.3, 0, 0, 1, 4, 3.3, 4.1};
 	ObstacleAvoidance* car2 = new ObstacleAvoidance(b);
 	cout << "Number of Groups: "<< car2->getGroups().size() << endl;
